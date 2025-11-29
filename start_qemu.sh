778665f8e5c916b7ed4aee40b946054b063b2a8a
hikaruvi <hikaruvixen@gmail.com>
2025-11-29 13:31:44 +0700

    manual select image

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-FIND_FILE_CMD="find ./romulus -name "*.static.mtd""
-IMAGE=$($FIND_FILE_CMD)
+# FIND_FILE_CMD="find ./romulus -name "*.static.mtd""
+# IMAGE=$($FIND_FILE_CMD)
+# echo $IMAGE
 
 # mkdir -p ./tmp
 
 qemu-system-arm -m 256 -M romulus-bmc \
--nographic -drive file=$IMAGE,format=raw,if=mtd \
+-nographic -drive file=./romulus/obmc-phosphor-image-romulus-20250916112422.static.mtd,format=raw,if=mtd \
 -net nic \
 -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::2443-:443,hostfwd=udp::2623-:623,hostname=qemu \
 > /dev/null
